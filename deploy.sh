6e04c821a2fba6d05a5e495c3279d77683513f10
Maciej Krüger <mkg20001@gmail.com>
2018-04-07 16:55:52 +0200

    Add deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf deploy
+mkdir deploy
+cp -r dist index.html deploy
+mkdir -p deploy/node_modules/xterm/dist
+cp node_modules/xterm/dist/xterm.css deploy/node_modules/xterm/dist/xterm.css
+HASH=$(ipfs add -Qr deploy)
+LINK="/ipfs/$HASH"
+ipfs-dnslink-update cloudflare ipfs-cli.mkg20001.io "$LINK"
